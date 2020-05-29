6f14ef432d7a03591072630d378cad7e9bab2570
Yash Desai <yddc902@live.com>
2020-05-29 18:53:11 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,75 @@
+// capsule as a noun = 
+
+// to encapsule - verb
+
+
+
+// setter
+// getter
+
+
+// modifiers
+// 	access modifiers
+// 		public
+// 		private
+// 		protected
+// 		default
+// 	non-access modifiers
+// 		'final'
+// 		'abstract'
+// 		'static'
+
+// abstract class Student{
+// 	public String fname = "Yash";
+// 	public abstract String printName();
+// }
+// public class Main extends Student{
+	
+// 	public String printName(){
+// 			return fname;
+// 	}	
+// 	public static void main(String args[]){		
+// 		Main m  = new Main();	
+// 		System.out.println(m.printName());
+// 	}
+// }
+
+
+// interface Student{
+// 	public String printStudentName();
+// 	// public String setName();
+// }
+// interface Teacher{
+// 	public String printTeacherName();
+// 	// public String setName();
+// }
+
+
+// public class Main implements Student , Teacher{
+// 	public String printStudentName(){
+// 		return "Kishan";
+// 	}
+// 	public String printTeacherName(){
+// 		return "Yash";
+// 	}
+	
+// 	public static void main(String args[]){		
+// 		Main m = new Main();
+		
+// 		System.out.println(m.printStudentName()+"  "+m.printTeacherName());
+// 	}
+// }
+
+
+/*
+	single level   A extends B
+	multilevel     A extends B and B extends C
+	herarchical	   B extends A while C extends A  
+	x multiple     class A extends class B and class A extends class C
+					class A implements interface B and class A implements interface 
+*/
+
+
+
+
+
